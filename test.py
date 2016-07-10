d855f8805d4864a703d904b0eb94132c91b712fa
Kjetil Asdal <kjetil.asdal@gmail.com>
2016-07-10 23:09:32 +0200

    Cleanup of start conditions and exit criteria

--- test.py
+++ test.py
@@ -4,7 +4,7 @@ import unittest
 
 import OpenHAB    
 
-class General(unittest.TestCase):
+class Daylight(unittest.TestCase):
 
     def setUp(self):
         self.openhab = OpenHAB.OpenHAB('192.168.100.15')
@@ -12,20 +12,28 @@ class General(unittest.TestCase):
         self.openhab.postUpdate('Alarm', 'None')
 
     def test_daylight_on(self):
-        self.openhab.sendCommand('sSunrise', 'OFF')
+        # Start conditions
+        self.openhab.postUpdate('sSunrise', 'OFF')
+        self.openhab.postUpdate('sDaylight', 'OFF')
         time.sleep(1)
+        # Trigger
         self.openhab.sendCommand('sSunrise', 'ON')
         time.sleep(1)
+        # Exit criteria
         self.assertEqual(self.openhab.getItem('sDaylight')['state'], 'ON')
-        self.assertEqual(self.openhab.getItem('Event')['state'], 'Daylight started')
+        self.assertEqual(self.openhab.getItem('Alarm')['state'], 'None')
 
     def test_daylight_off(self):
-        self.openhab.sendCommand('sSunset', 'OFF')
+        # Start conditions
+        self.openhab.postUpdate('sSunset', 'OFF')
+        self.openhab.postUpdate('sDaylight', 'ON')
         time.sleep(1)
+        # Trigger
         self.openhab.sendCommand('sSunset', 'ON')
         time.sleep(1)
+        # Exit criteria
         self.assertEqual(self.openhab.getItem('sDaylight')['state'], 'OFF')
-        self.assertEqual(self.openhab.getItem('Event')['state'], 'Daylight ended')
+        self.assertEqual(self.openhab.getItem('Alarm')['state'], 'None')
 
 class HouseMode(unittest.TestCase):
 
@@ -35,24 +43,48 @@ class HouseMode(unittest.TestCase):
         self.openhab.postUpdate('Alarm', 'None')
 
     def test_home(self):
+        # Start conditions
+        self.openhab.postUpdate('sHome', 'OFF')
+        time.sleep(1)
+        # Trigger
         self.openhab.sendCommand('sHome', 'ON')
         time.sleep(1)
+        # Exit criteria
         self.assertEqual(self.openhab.getItem('Event')['state'], 'House mode changed to HOME')
+        self.assertEqual(self.openhab.getItem('Alarm')['state'], 'None')
 
     def test_away(self):
+        # Start conditions
+        self.openhab.postUpdate('sHome', 'ON')
+        time.sleep(1)
+        # Trigger
         self.openhab.sendCommand('sHome', 'OFF')
         time.sleep(1)
+        # Exit criteria
         self.assertEqual(self.openhab.getItem('Event')['state'], 'House mode changed to AWAY')
+        self.assertEqual(self.openhab.getItem('Alarm')['state'], 'None')
 
     def test_awake(self):
+        # Start conditions
+        self.openhab.postUpdate('sAwake', 'OFF')
+        time.sleep(1)
+        # Trigger
         self.openhab.sendCommand('sAwake', 'ON')
         time.sleep(1)
+        # Exit criteria
         self.assertEqual(self.openhab.getItem('Event')['state'], 'House mode changed to AWAKE')
+        self.assertEqual(self.openhab.getItem('Alarm')['state'], 'None')
 
     def test_sleep(self):
+        # Start conditions
+        self.openhab.postUpdate('sAwake', 'ON')
+        time.sleep(1)
+        # Trigger
         self.openhab.sendCommand('sAwake', 'OFF')
         time.sleep(1)
+        # Exit criteria
         self.assertEqual(self.openhab.getItem('Event')['state'], 'House mode changed to SLEEP')
+        self.assertEqual(self.openhab.getItem('Alarm')['state'], 'None')
 
 class Ligthing(unittest.TestCase):
 
@@ -62,38 +94,67 @@ class Ligthing(unittest.TestCase):
         self.openhab.postUpdate('Alarm', 'None')
 
     def test_outdoor_on(self):
+        # Start conditions
+        self.openhab.postUpdate('sDaylight', 'ON')
+        self.openhab.postUpdate('S01D006_sSwitch', 'OFF')
+        self.openhab.postUpdate('S01D006_sMonitor', 'OFF')
+        self.openhab.postUpdate('S01D006_nPower', '0') # No power draw
+        self.openhab.postUpdate('S01D007_sSwitch', 'OFF')
+        self.openhab.postUpdate('S01D007_sMonitor', 'OFF')
+        self.openhab.postUpdate('S01D007_nPower', '0') # No power draw        
+        # Trigger
         self.openhab.sendCommand('sDaylight', 'OFF')
-        time.sleep(1)
-        self.assertEqual(self.openhab.getItem('S01D006_sSwitch')['state'], 'ON')
-        self.assertEqual(self.openhab.getItem('S01D007_sSwitch')['state'], 'ON')
         self.openhab.postUpdate('S01D006_nPower', '40') # Normal power draw
         self.openhab.postUpdate('S01D007_nPower', '32') # Normal power draw
-        time.sleep(61)
+        time.sleep(60 + 5)
+        # Exit criteria
+        self.assertEqual(self.openhab.getItem('S01D006_sSwitch')['state'], 'ON')
         self.assertEqual(self.openhab.getItem('S01D006_sMonitor')['state'], 'ON')
+        self.assertEqual(self.openhab.getItem('S01D007_sSwitch')['state'], 'ON')
         self.assertEqual(self.openhab.getItem('S01D007_sMonitor')['state'], 'ON')
         self.assertEqual(self.openhab.getItem('Alarm')['state'], 'None')
 
     def test_outdoor_on_lowpower(self):
+        # Start conditions
+        self.openhab.postUpdate('sDaylight', 'ON')
+        self.openhab.postUpdate('S01D006_sSwitch', 'OFF')
+        self.openhab.postUpdate('S01D006_sMonitor', 'OFF')
+        self.openhab.postUpdate('S01D006_nPower', '0') # No power draw
+        self.openhab.postUpdate('S01D007_sSwitch', 'OFF')
+        self.openhab.postUpdate('S01D007_sMonitor', 'OFF')
+        self.openhab.postUpdate('S01D007_nPower', '0') # No power draw        
+        # Trigger
         self.openhab.sendCommand('sDaylight', 'OFF')
-        time.sleep(1)
-        self.assertEqual(self.openhab.getItem('S01D006_sSwitch')['state'], 'ON')
-        self.assertEqual(self.openhab.getItem('S01D007_sSwitch')['state'], 'ON')
         self.openhab.postUpdate('S01D006_nPower', '40') # Normal power draw
         self.openhab.postUpdate('S01D007_nPower', '16') # Lower than normal power draw
-        time.sleep(61)
+        time.sleep(60 + 5)
+        # Exit criteria
+        self.assertEqual(self.openhab.getItem('S01D006_sSwitch')['state'], 'ON')
         self.assertEqual(self.openhab.getItem('S01D006_sMonitor')['state'], 'ON')
+        self.assertEqual(self.openhab.getItem('S01D007_sSwitch')['state'], 'ON')
         self.assertEqual(self.openhab.getItem('S01D007_sMonitor')['state'], 'ON')
         self.assertEqual(self.openhab.getItem('Alarm')['state'], 'Outdoor lights (south) draw less power than normal')
 
     def test_outdoor_off(self):
+        # Start conditions
+        self.openhab.postUpdate('sDaylight', 'OFF')
+        self.openhab.postUpdate('S01D006_sSwitch', 'ON')
+        self.openhab.postUpdate('S01D006_sMonitor', 'ON')
+        self.openhab.postUpdate('S01D006_nPower', '40') # Normal power draw
+        self.openhab.postUpdate('S01D007_sSwitch', 'ON')
+        self.openhab.postUpdate('S01D007_sMonitor', 'ON')
+        self.openhab.postUpdate('S01D007_nPower', '32') # Normal power draw        
+        # Trigger
         self.openhab.sendCommand('sDaylight', 'ON')
-        time.sleep(1)
-        self.assertEqual(self.openhab.getItem('S01D006_sSwitch')['state'], 'OFF')
-        self.assertEqual(self.openhab.getItem('S01D007_sSwitch')['state'], 'OFF')
-        self.assertEqual(self.openhab.getItem('S01D006_sMonitor')['state'], 'OFF')
-        self.assertEqual(self.openhab.getItem('S01D007_sMonitor')['state'], 'OFF')
         self.openhab.postUpdate('S01D006_nPower', '0') # No power draw
         self.openhab.postUpdate('S01D007_nPower', '0') # No power draw
+        time.sleep(1)
+        # Exit criteria
+        self.assertEqual(self.openhab.getItem('S01D006_sSwitch')['state'], 'OFF')
+        self.assertEqual(self.openhab.getItem('S01D006_sMonitor')['state'], 'OFF')
+        self.assertEqual(self.openhab.getItem('S01D007_sSwitch')['state'], 'OFF')
+        self.assertEqual(self.openhab.getItem('S01D007_sMonitor')['state'], 'OFF')
+        self.assertEqual(self.openhab.getItem('Alarm')['state'], 'None')
 
 class Presence(unittest.TestCase):
 
@@ -103,7 +164,7 @@ class Presence(unittest.TestCase):
         self.openhab.postUpdate('Alarm', 'None')
 
     def test_wasp_in(self):
-        # Initial values
+        # Start conditions
         self.openhab.postUpdate('cDoor', 'CLOSED')
         self.openhab.postUpdate('cMotion', 'CLOSED')
         self.openhab.postUpdate('sWasp', 'OFF')
@@ -111,10 +172,12 @@ class Presence(unittest.TestCase):
         # Trigger
         self.openhab.postUpdate('cMotion', 'OPEN')
         time.sleep(1)
+        # Exit criteria
         self.assertEqual(self.openhab.getItem('sWasp')['state'], 'ON')
+        self.assertEqual(self.openhab.getItem('Alarm')['state'], 'None')
         
     def test_wasp_out(self):
-        # Initial values
+        # Start conditions
         self.openhab.postUpdate('cDoor', 'CLOSED')
         self.openhab.postUpdate('cMotion', 'CLOSED')
         self.openhab.postUpdate('sWasp', 'ON')
@@ -122,10 +185,12 @@ class Presence(unittest.TestCase):
         # Trigger
         self.openhab.postUpdate('cDoor', 'OPEN')
         time.sleep(1)
+        # Exit criteria
         self.assertEqual(self.openhab.getItem('sWasp')['state'], 'OFF')
+        self.assertEqual(self.openhab.getItem('Alarm')['state'], 'None')
 
     def test_wasp_in_out(self):
-        # Initial values
+        # Start conditions
         self.openhab.postUpdate('cDoor', 'OPEN')
         self.openhab.postUpdate('cMotion', 'CLOSED')
         self.openhab.postUpdate('sWasp', 'OFF')
@@ -133,7 +198,9 @@ class Presence(unittest.TestCase):
         # Trigger
         self.openhab.postUpdate('cMotion', 'OPEN')
         time.sleep(1)
+        # Exit criteria
         self.assertEqual(self.openhab.getItem('sWasp')['state'], 'OFF')
+        self.assertEqual(self.openhab.getItem('Alarm')['state'], 'None')
 
 if __name__ == '__main__':
     unittest.main()
